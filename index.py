e9b460cdc9a54060d5cbdaec59cf8638ec8fd5c0
tefan33 <tefan33@gmail.com>
2025-02-16 16:24:09 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -31,4 +31,4 @@ def display_page(pathname):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8050))
-    app.run_server(host='0.0.0.0', port=port)
+    app.run_server(host='0.0.0.0', port=port, debug=False)
