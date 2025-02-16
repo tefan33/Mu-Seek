3cf6e884cb9ccbec04ccfc63754fc64c01e49c32
tefan33 <tefan33@gmail.com>
2025-02-16 16:55:35 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -1,20 +1,34 @@
-from dash import Dash, html
+from dash import html, dcc
+from dash.dependencies import Input, Output
+from home import create_page_home
+from page_2 import create_page_2
+from page_3 import create_page_3
+from page_4 import create_page_4
+from app import app
+import os 
 
-# Créer une instance de l'application Dash
-app = Dash(__name__)
-# Exposer l'objet server pour Gunicorn
 server = app.server
+app.config.suppress_callback_exceptions = True
 
-# Définir la mise en page de l'application
-app.layout = html.Div(
-    children=[
-        html.H1("Bonjour"),
-        html.P("Bienvenue sur cette page Dash simple !")
-    ],
-    style={'textAlign': 'center', 'marginTop': '50px'}
-)
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
 
 
-# Exécuter l'application
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page-2':
+        return create_page_2()
+    if pathname == '/page-3':
+        return create_page_3()
+    if pathname == '/page-4':
+        return create_page_4()
+    else:
+        return create_page_home()
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)
